1d5e7dce2e05096c2c79119f6878d2f51a22722e
Sainjurohan <Sainju34@gmail.com>
2024-01-23 12:47:27 +0545

    feat: Update Route for Blogs in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -35,4 +36,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// adding route for blogs
+Route::resource('blogs', BlogController::class);
 require __DIR__.'/auth.php';
